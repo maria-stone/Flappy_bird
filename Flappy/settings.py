1ec72b420280e29bb3dfe95fc50a5f31609613f9
maria-stone <tieiter@tsn.at>
2026-06-15 10:18:11 +0200

    Add files via upload

---
+++ Flappy/settings.py
@@ -0,0 +1,34 @@
+# Fenster
+WIDTH  = 400
+HEIGHT = 600
+FPS    = 60 #frames per sekound 
+
+# Physik
+GRAVITY    = 0.5 
+JUMP_FORCE = -8
+
+# Rohre
+PIPE_SPEED    = 3
+PIPE_GAP      = 150
+PIPE_WIDTH    = 60
+PIPE_INTERVAL = 1500
+
+# Boden
+GROUND_HEIGHT = 60
+
+# Farben
+SKY        = (78,  192, 202)
+WHITE      = (255, 255, 255)
+BLACK      = (0,   0,   0)
+GROUND_COL = (210, 180, 100)
+GRASS_COL  = (100, 180,  80)
+PIPE_COL   = (83,  185,  80)
+PIPE_DARK  = (60,  140,  60)
+YELLOW     = (255, 220,  50)
+ORANGE     = (255, 160,  30)
+RED        = (220,  50,  50)
+GRAY       = (180, 180, 180)
+# zusätzliche Farben für Charakterauswahl
+BLUE   = (60,  120, 220)
+GREEN  = (80,  200, 100)
+PURPLE = (160,  90, 210)
